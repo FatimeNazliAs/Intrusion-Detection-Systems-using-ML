e45b13bb50f9e5d42ab879d43876413911f75f74
Nazlı As <f.nazlias@gmail.com>
2024-12-16 15:37:37 +0300

    Deleted unnecessary lines

--- src/utils.py
+++ src/utils.py
@@ -10,8 +10,6 @@ def load_data_paths_from_config(path_name):
     dos_df_path = os.path.join(dir, paths.dos_df).replace("\\", "/")
     fuzzy_df_path = os.path.join(dir, paths.fuzzy_df).replace("\\", "/")
     attack_free_df_path = os.path.join(dir, paths.attack_free_df).replace("\\", "/")
-    # print(dos_df_path)
-    # print(fuzzy_df_path)
     return dos_df_path, fuzzy_df_path, attack_free_df_path
 
 
